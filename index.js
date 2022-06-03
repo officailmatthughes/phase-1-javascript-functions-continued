c4d0cbd5ef68cfe45bc2b17c5f88275b925ccaad
Mattherw <officialmatthughes>
2022-06-02 20:53:20 -0700

    commit tree bfcc36342f524418c676eeeb244516483f79dcaf

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(funPhrase = "roller-skate") {
+    return `This Saturday, I want to ${funPhrase}!`;
+}
+
+function mondayWork(nextPhrase = "go to the office") {
+    return `This Monday, I will ${nextPhrase}.`
+}
+
+function wrapAdjective(adj = "*") {
+    return function (newPhrase) {
+        return `You are ${adj}${newPhrase}${adj}!`
+    }
+}
